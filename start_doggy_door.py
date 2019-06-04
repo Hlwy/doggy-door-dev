67249d7b6635ffe8fd4663d6451b7eb9d4356c4a
Hunter Young <hunter.lw.young@gmail.com>
2019-06-04 07:29:39 -0400

    removed un needed comments

--- start_doggy_door.py
+++ start_doggy_door.py
@@ -88,14 +88,3 @@ if __name__ == "__main__":
     upSwitch.close()
     lowSwitch.close()
     pl.close()
-
-    # pl = BLEDevicePoller(flag_hw_reset=True)
-    # pl.add_device("BlueCharm","B0:91:22:F7:6D:55",'bluecharm')
-    # pl.add_device("tkr","C3:CE:5E:26:AD:0A",'trackr')
-    #
-    # pl.start_btmon()
-    # pl.update_thread = threading.Thread(target=pl.update_devices)
-    # pl.motor_thread = threading.Thread(target=pl.motor_loop)
-    # pl.update_thread.start()
-    # pl.motor_thread.start()
-    # pl.loop()
