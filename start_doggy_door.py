e69416e99c20e2b067e78ea131e00eea377cbb78
Hunter Young <hunter.lw.young@gmail.com>
2019-06-04 06:17:25 -0400

    arg fix

--- start_doggy_door.py
+++ start_doggy_door.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
 
     # Setup commandline argument(s) structures
     ap = argparse.ArgumentParser(description='Pi Rotary Encoder')
-    ap.add_argument("--upper-limit", "-u", type=int, default=21, metavar='GPIO', help="Gpio responsible for setting motor direction")
-    ap.add_argument("--lower-limit", "-l", type=int, default=20, metavar='GPIO', help="Pin responsible for setting motor PWM signal")
+    ap.add_argument("--upper", "-u", type=int, default=21, metavar='GPIO', help="Gpio responsible for setting motor direction")
+    ap.add_argument("--lower", "-l", type=int, default=20, metavar='GPIO', help="Pin responsible for setting motor PWM signal")
     ap.add_argument("--sleep", "-t", type=int, default=300, metavar='PERIOD', help="How long you want the program to run (secs)")
     # Store parsed arguments into array of variables
     args = vars(ap.parse_args())
 
     # Extract stored arguments array into individual variables for later usage in script
-    upLim = args["upper-limit"]
-    lowLim = args["lower-limit"]
+    upLim = args["upper"]
+    lowLim = args["lower"]
     dt = args["sleep"]
 
     pi = pigpio.pi()
