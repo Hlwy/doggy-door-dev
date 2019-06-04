75afedf2c944f0de7ead0a83b84c82448d4dd758
Hunter Young <hunter.lw.young@gmail.com>
2019-06-03 20:03:47 -0400

    semi functional proximity checker

--- scripts/ble_device_poller.py
+++ scripts/ble_device_poller.py
@@ -7,7 +7,9 @@ import signal
 pp = pprint.PrettyPrinter(indent=4)
 
 class BLEDevicePoller(object):
-    def __init__(self):
+    def __init__(self, flag_hw_reset=False):
+        if flag_hw_reset: self.restart_hardware()
+
         signal.signal(signal.SIGINT, self.signal_handler)
         self.btmon = None
         self.lescan = None
@@ -24,6 +26,7 @@ class BLEDevicePoller(object):
         self.init_time = time.time()
         self.last_time = time.time()
         self.paired_devices = []
+        self.found_devices = []
         self.previous_devices = []
 
         self.update_rate = 0.1
@@ -78,6 +81,21 @@ class BLEDevicePoller(object):
         if not is_found:
             print("[WARNING] Could not find a device with id = '%s' within list of known devices." % (id))
 
+    def restart_hardware(self,verbose=True):
+        if(verbose): print("[INFO] Turning off 'hci0' hardware interface...")
+        try: sp.call("sudo hciconfig hci0 down".split(" "))
+        except:
+            print("[ERROR] Could not shut off 'hci0' hardware interface.")
+            pass
+        time.sleep(1.0)
+        if(verbose): print("[INFO] Turning on 'hci0' hardware interface...")
+        try: sp.call("sudo hciconfig hci0 up".split(" "))
+        except:
+            print("[ERROR] Could not turn on 'hci0' hardware interface.")
+            pass
+        print("[INFO] Finished resetting bluetooth hardware.")
+        time.sleep(1.0)
+
     def start_btmon(self,verbose=True):
         if(verbose): print("[INFO] Starting 'btmon' process...")
         self.btmon = sp.Popen("sudo btmon".split(" "),stdout=sp.PIPE,stderr=sp.PIPE)
@@ -130,13 +148,12 @@ class BLEDevicePoller(object):
     def update_devices(self):
         count = 0
         last_count = 0
-        # lastDevs = []
         if self.btmon is not None:
+            self.found_devices = list(self.paired_devices)
             for line in iter(self.btmon.stdout.readline, b''):
-                self.lock.acquire()
-
                 count+=1
-                copyDevs = self.paired_devices
+                self.lock.acquire()
+                copyDevs = list(self.found_devices)
                 for i,dev in enumerate(copyDevs):
                     if dev["addr"] in line.rstrip():
                         dev["times_seen"]+=1
@@ -147,7 +164,7 @@ class BLEDevicePoller(object):
                     if tmpRssi and (count - dev["last_count"]) < 15:
                         dev["rssi"] = tmpRssi
 
-                self.paired_devices = copyDevs
+                self.found_devices = copyDevs
                 self.nUpdates+=1
                 self.lock.release()
                 time.sleep(0)
@@ -159,25 +176,29 @@ class BLEDevicePoller(object):
 
     def get_devices(self):
         self.lock.acquire()
-        curDevs = self.paired_devices
+        foundDevs = list(self.found_devices)
         self.lock.release()
-        return curDevs
+        return foundDevs
 
-    def loop(self, dt=30, check_period=1.0):
+    def loop(self, dt=30, check_period=0.5,verbose=True):
         self.last_time = time.time()
-        prevDevs = []
         lastSeen = []
         testT = time.time()
+        prevDevs = None
+        prevDev = None
         while 1:
             self.nLoops+=1
             now = time.time()
             self.dt = now - self.last_time
             curDevs = self.get_devices()
-            if lastSeen != curDevs[0]["times_seen"]:
-                lastSeen = curDevs[0]["times_seen"]
-                test_dt = time.time() - testT
-                print("updated values of [%d] took %s secs" % (curDevs[0]["times_seen"],str(test_dt)))
-                testT = time.time()
+            # prevDevs = self.previous_devices
+
+            # if lastSeen != curDevs[0]["times_seen"]:
+            #     lastSeen = curDevs[0]["times_seen"]
+            #     test_dt = time.time() - testT
+            #     print("updated values of [%d] took %s secs" % (curDevs[0]["times_seen"],str(test_dt)))
+            #     testT = time.time()
+
             # self.check_proximity(curDevs, prevDevs)
             # prevDevs = curDevs
 
@@ -185,11 +206,27 @@ class BLEDevicePoller(object):
             if self.dt >= check_period:
                 self.last_time = time.time()
                 self.nChecks+=1
+
+                if prevDevs is None: prevDevs = [dict(d) for d in curDevs]
+
+
+                self.check_proximity(curDevs,prevDevs)
+
+                # if prevDev is None: prevDev = dict(curDev)
+                # print(curDev["times_seen"],prevDev["times_seen"])
+                # prevDev = dict(curDev)
+                prevDevs = [dict(d) for d in curDevs]
+
+                # if len(prevDevs) <= 0: prevDevs.append(dict(curDevs[0]))
+                # else: prevDevs[0] = dict(curDevs[0])
+
                 # curDevs = self.get_devices()
-                self.check_proximity(curDevs, prevDevs,verbose=False)
-                prevDevs = curDevs
+                # self.check_proximity(curDevs, prevDevs,verbose=False)
+                # prevDev = curDev
                 # self.check_proximity(self.get_devices())
                 # self.previous_devices = curDevs
+                # oldTest = newTest
+
             # Check for exit conditions
             if self.dt >= dt:
                 print("[%.2f] Exiting loop." % now)
@@ -200,36 +237,48 @@ class BLEDevicePoller(object):
             if self.flag_exit_update: break
         print("[INFO] loop() --- Loop exited. Initializing shut down...")
         self.flag_exit_update = True
+        self.__del__()
 
-    def check_proximity(self, curDevs,prevDevs, verbose=True):
+    def check_proximity(self, curDevs,prevDevs, verbose=False):
         is_device_in_range = False
         nNearDevices = 0
-        if len(prevDevs) == 0: return is_device_in_range
+        if len(prevDevs) <= 0: return is_device_in_range
+
+        # if len(prevDevs) > 0:
+        #     for i,pd in enumerate(prevDevs):
+        #         if(verbose): print("Checking current devices with [%s]:" % (pd["addr"]) )
+        #         for j,cd in enumerate(curDevs):
+        #             if cd["addr"] == pd["addr"]:
+        #                 print(cd["times_seen"],pd["times_seen"])
+
 
         for i in range(self.nDevices):
-            if verbose:
-                print(curDevs[i])
-                print(" ------------ ")
-                print(prevDevs[i])
-                print(" ============ ")
+            # if verbose:
+            #     print(curDevs[i])
+            #     print(" ------------ ")
+            #     print(prevDevs[i])
+            #     print(" ============ ")
 
             if curDevs[i]["times_seen"] == prevDevs[i]["times_seen"]:
                 if False: print("[INFO] check_proximity() ---- No new updates for device [%d], skipping...." % (i))
             else:
+                print("Device [%s] detected with RSSI = %d" % (curDevs[i]["addr"],curDevs[i]["rssi"]))
                 tmpVal = curDevs[i]["rssi"]
+                # is_device_in_range = True
                 if tmpVal >= -70:
                     is_device_in_range = True
                     nNearDevices+=1
 
         # tmpStr = str(dev["name"] + ": [nSeen=" + str(dev["times_seen"]) + ", RSSI=" + str(dev["rssi"])+", lastCount=" + str(dev["last_count"]) + "]")
         # print(tmpStr)
-        if verbose:
-            if not is_device_in_range: print("[INFO] check_proximity() ---- No Devices in range.")
-            else: print("[INFO] check_proximity() ---- %d Devices found in range. BEGIN DOOR OPEN SEQUENCE." % (nNearDevices))
+
+        if not is_device_in_range:
+            if verbose: print("[INFO] check_proximity() ---- No Devices in range.")
+        else: print("[INFO] check_proximity() ---- %d Devices found in range. BEGIN DOOR OPEN SEQUENCE." % (nNearDevices))
         return is_device_in_range
 
 if __name__ == "__main__":
-    pl = BLEDevicePoller()
+    pl = BLEDevicePoller(flag_hw_reset=True)
     pl.add_device("BlueCharm","B0:91:22:F7:6D:55",0)
     # pl.add_device("tkr","C3:CE:5E:26:AD:0A",1)
 
